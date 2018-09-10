bee250b927c867fb388c6632687d853ea6e7cfd8
Yash Kulshreshtha <yash.kulshreshtha.yash@hotmail.com>
2018-09-10 20:24:18 +0530

    enable pwa

--- yuvacentral/urls.py
+++ yuvacentral/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.password_reset_confirm, {'template_name':'password_reset_confirm.html'}, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete, {'template_name':'password_reset_complete.html'}, name='password_reset_complete'),
+    url('', include('pwa.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
