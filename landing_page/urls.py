dec8729cce44fa85d6d0ae80c07a41903c51e9c9
Yash Kulshreshtha <yash.kulshreshtha.yash@hotmail.com>
2018-09-25 10:44:57 +0530

    login url change

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', home, name = 'home'),
-    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^login/login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$',auth_views.logout, {'next_page': 'landing:login'}, name = 'logout'),
     ]
