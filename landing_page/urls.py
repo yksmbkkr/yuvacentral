5f710ff821eb2a0e2b9634d78015e07547a42615
Yash Kulshreshtha <yash.kulshreshtha.yash@hotmail.com>
2018-09-25 11:06:05 +0530

    login url change

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
-from .views import home
+from .views import home, login_redirect
 from django.contrib.auth import views as auth_views
 
 
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^$', home, name = 'home'),
     url(r'^login/login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$',auth_views.logout, {'next_page': 'landing:login'}, name = 'logout'),
+    url(r'^login/$', login_redirect),
     ]
--- landing_page/views.py
+++ landing_page/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from manage_dashboard import models as m_models
 
 # Create your views here.
@@ -9,3 +9,9 @@ def home(request):
     if upcoming_event_list.count()>0:
         elist=True
     return render(request, 'yuva.html',{'up_eve':upcoming_event_list,'elist':elist})
+
+def login_redirect(request):
+    next_route = request.GET.get('next')
+    if not next_route:
+        return redirect('landing:login')
+    return redirect('/login/login/?next='+next_route)
