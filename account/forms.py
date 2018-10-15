8720e90d90aa2b1eb29de0f888fa8abd58d2cdef
Yash Kulshreshtha <yash.kulshreshtha.yash@hotmail.com>
2018-10-15 22:20:51 +0530

    username pattern

--- account/forms.py
+++ account/forms.py
@@ -22,7 +22,7 @@ class profession_choice_form(forms.Form):
 
 
 class registration_form(UserCreationForm):
-    username = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Username', 'pattern':'[a-zA-Z0-9@.]{1,30}'}))
+    username = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Username', 'pattern':'[a-zA-Z0-9@.]{1,30}','title':'Username should not contain spaces. It could be alphanumeric only.'}))
     email = forms.EmailField(max_length=254, required=True, help_text='Required. Inform a valid email address.', widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Retype Password'}))
